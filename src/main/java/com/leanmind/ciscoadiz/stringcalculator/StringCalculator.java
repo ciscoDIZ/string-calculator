75cb92c5ea0b7976e63c88d5a6d6c44a58840514
ciscoadiz <ciscoadiz@gmail.com>
2022-04-06 15:18:50 +0100

    negative numbers not allowed

--- src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
+++ src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
@@ -1,23 +1,58 @@
 package com.leanmind.ciscoadiz.stringcalculator;
 
+import com.sun.source.tree.UsesTree;
+
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class StringCalculator {
-    public int addition(String expression) {
+
+    private String separator;
+
+    public StringCalculator() {
+        separator = ",";
+    }
+
+    private void errorHandler(String expression) throws Exception {
+       int[] result = Stream.of(expression.split(separator))
+               .mapToInt(Integer::parseInt)
+               .filter(n -> n < 0)
+               .toArray();
+       if (result.length > 0) {
+           throw new Exception("not allowed negatives: " + Arrays.toString(result));
+       }
+    }
+    public int addition(String expression) throws Exception {
 
         if (expression.isEmpty()) {
             return 0;
         }
+
         if (expression.contains("\n")) {
+            String pattern = "(//)([;:,])";
             String[] splitExpression = expression.split("\n");
+            if (splitExpression[0].matches(pattern)) {
+                Pattern regExp = Pattern.compile(pattern);
+                Matcher matcher = regExp.matcher(splitExpression[0]);
+                if (matcher.find()){
+                    separator = matcher.group(2);
+                }
+                splitExpression = Stream.of(splitExpression)
+                        .filter(s -> !s.matches(pattern))
+                        .toArray(String[]::new);
+            }
             String mergedExpression = "";
             for (String line : splitExpression) {
-                mergedExpression = mergedExpression.concat(line).concat(",");
+                mergedExpression = mergedExpression.concat(line).concat(separator);
             }
             expression = mergedExpression.substring(0, mergedExpression.length() - 1);
         }
-
-        return Stream.of(expression.split(",")).mapToInt(Integer::parseInt).sum();
+        errorHandler(expression);
+        return Stream.of(expression.split(separator))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -6,21 +6,31 @@ import static org.junit.Assert.*;
 
 public class StringCalculatorShould {
     @Test
-    public void add_two_numbers_separated_by_commas() {
+    public void add_two_numbers_separated_by_commas() throws Exception {
         int expected = 3;
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(expected, stringCalculator.addition("1,2"));
     }
     @Test
-    public void for_empty_string_should_not_add() {
+    public void for_empty_string_should_not_add() throws Exception {
         int expected = 0;
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(expected, stringCalculator.addition(""));
     }
     @Test
-    public void allow_to_handle_new_lines_between_numbers() {
+    public void allow_to_handle_new_lines_between_numbers() throws Exception {
         int expected = 6;
         assertEquals(expected, new StringCalculator().addition("1\n2,3"));
+    }
+    @Test
+    public void support_different_delimiters() throws Exception {
+        int expected = 3;
+        assertEquals(expected, new StringCalculator().addition("//;\n1;2"));
+    }
+    @Test
+    public void negative_numbers_not_allowed() {
+        Throwable exception = assertThrows(Exception.class, () -> new StringCalculator().addition("-1,5,-8"));
+        assertEquals("not allowed negatives: [-1, -8]", exception.getMessage());
     }
 
 }

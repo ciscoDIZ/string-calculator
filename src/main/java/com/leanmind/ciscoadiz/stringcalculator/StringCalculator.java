a18d0cb31d9c03b3ab58d260e9e7ffccdeb1476d
ciscoadiz <ciscoadiz@gmail.com>
2022-04-06 13:15:44 +0100

    allow handling new lines between numbers

--- src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
+++ src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
@@ -5,9 +5,19 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
     public int addition(String expression) {
+
         if (expression.isEmpty()) {
             return 0;
         }
+        if (expression.contains("\n")) {
+            String[] splitExpression = expression.split("\n");
+            String mergedExpression = "";
+            for (String line : splitExpression) {
+                mergedExpression = mergedExpression.concat(line).concat(",");
+            }
+            expression = mergedExpression.substring(0, mergedExpression.length() - 1);
+        }
+
         return Stream.of(expression.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -17,4 +17,10 @@ public class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(expected, stringCalculator.addition(""));
     }
+    @Test
+    public void allow_to_handle_new_lines_between_numbers() {
+        int expected = 6;
+        assertEquals(expected, new StringCalculator().addition("1\n2,3"));
+    }
+
 }

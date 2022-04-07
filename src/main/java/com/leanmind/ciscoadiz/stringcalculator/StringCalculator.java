a084ccae7ec85d7f8af3ab6eafca9efed6fa83fb
ciscoadiz <ciscoadiz@gmail.com>
2022-04-07 17:17:31 +0100

    delimiters can be of any length

--- src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
+++ src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
@@ -1,9 +1,6 @@
 package com.leanmind.ciscoadiz.stringcalculator;
 
-import com.sun.source.tree.UsesTree;
-
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -17,42 +14,63 @@ public class StringCalculator {
     }
 
     private void errorHandler(String expression) throws Exception {
-       int[] result = Stream.of(expression.split(separator))
-               .mapToInt(Integer::parseInt)
-               .filter(n -> n < 0)
-               .toArray();
-       if (result.length > 0) {
-           throw new Exception("not allowed negatives: " + Arrays.toString(result));
-       }
+        int[] result = Stream.of(expression.split(separator))
+                .mapToInt(Integer::parseInt)
+                .filter(n -> n < 0)
+                .toArray();
+        if (result.length > 0) {
+            throw new Exception("not allowed negatives: " + Arrays.toString(result));
+        }
     }
-    public int addition(String expression) throws Exception {
 
+
+
+    public int operate(String expression) throws Exception {
         if (expression.isEmpty()) {
             return 0;
         }
-
         if (expression.contains("\n")) {
-            String pattern = "(//)([;:,])";
-            String[] splitExpression = expression.split("\n");
-            if (splitExpression[0].matches(pattern)) {
-                Pattern regExp = Pattern.compile(pattern);
-                Matcher matcher = regExp.matcher(splitExpression[0]);
-                if (matcher.find()){
-                    separator = matcher.group(2);
-                }
-                splitExpression = Stream.of(splitExpression)
-                        .filter(s -> !s.matches(pattern))
-                        .toArray(String[]::new);
-            }
-            String mergedExpression = "";
-            for (String line : splitExpression) {
-                mergedExpression = mergedExpression.concat(line).concat(separator);
-            }
-            expression = mergedExpression.substring(0, mergedExpression.length() - 1);
+            expression = determinateSeparator(expression);
         }
-        errorHandler(expression);
-        return Stream.of(expression.split(separator))
-                .mapToInt(Integer::parseInt)
-                .sum();
+        String[] splitExpression = expression.split("\n");
+        return addition(splitExpression);
+    }
+
+    private int addition(String[] expressions) throws Exception {
+        int result = 0;
+        for (String expression : expressions) {
+            errorHandler(expression);
+            result += Stream.of(expression.split(separator))
+                    .mapToInt(Integer::parseInt)
+                    .filter(n -> n <= 1000)
+                    .sum();
+        }
+
+        return result;
+    }
+
+    private String determinateSeparator(String expression) {
+        String[] splitExpressionLines = expression.split("\n");
+        String separatorPattern = "(//)(\\[?[\\D]+]?)";
+        if (splitExpressionLines[0].matches(separatorPattern)) {
+            Pattern regExp = Pattern.compile(separatorPattern);
+            Matcher matcher = regExp.matcher(splitExpressionLines[0]);
+            if (matcher.find()) {
+                separator = matcher.group(2);
+                String escapableCharacter = "(\\*|\\+|\\.|\\^|\\{|\\||\\[|\\)|\\(\\?|])+";
+                if (separator.matches(escapableCharacter)) {
+                    StringBuilder separatorBuilder = new StringBuilder();
+                    separator = separatorBuilder
+                            .append("\\")
+                            .append(separator.charAt(1))
+                            .append("+")
+                            .toString();
+                }
+            }
+            //System.out.println(Arrays.toString(splitExpressionLines));
+            expression = splitExpressionLines[1];
+
+        }
+        return expression;
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -9,28 +9,37 @@ public class StringCalculatorShould {
     public void add_two_numbers_separated_by_commas() throws Exception {
         int expected = 3;
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(expected, stringCalculator.addition("1,2"));
+        assertEquals(expected, stringCalculator.operate("1,2"));
     }
     @Test
     public void for_empty_string_should_not_add() throws Exception {
         int expected = 0;
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(expected, stringCalculator.addition(""));
+        assertEquals(expected, stringCalculator.operate(""));
     }
     @Test
     public void allow_to_handle_new_lines_between_numbers() throws Exception {
         int expected = 6;
-        assertEquals(expected, new StringCalculator().addition("1\n2,3"));
+        assertEquals(expected, new StringCalculator().operate("1\n2,3"));
     }
     @Test
     public void support_different_delimiters() throws Exception {
         int expected = 3;
-        assertEquals(expected, new StringCalculator().addition("//;\n1;2"));
+        assertEquals(expected, new StringCalculator().operate("//;\n1;2"));
     }
     @Test
     public void negative_numbers_not_allowed() {
-        Throwable exception = assertThrows(Exception.class, () -> new StringCalculator().addition("-1,5,-8"));
+        Throwable exception = assertThrows(Exception.class, () -> new StringCalculator().operate("-1,5,-8"));
         assertEquals("not allowed negatives: [-1, -8]", exception.getMessage());
     }
-
+    @Test
+    public void numbers_bigger_than_1000_should_be_ignored() throws Exception {
+        int expected = 2;
+        assertEquals(expected, new StringCalculator().operate("1001,2"));
+    }
+    @Test
+    public void delimiters_can_be_of_any_length() throws Exception {
+        int expected = 6;
+        assertEquals(expected, new StringCalculator().operate("//[***]\n1***2***3"));
+    }
 }

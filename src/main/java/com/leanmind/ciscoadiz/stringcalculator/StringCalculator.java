f9026878b41e66baf2bc1f28eefbacd4bfc7da9f
ciscoadiz <ciscoadiz@gmail.com>
2022-04-08 03:39:04 +0100

    operate can take up to two numbers separated by commas

--- src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
+++ src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
@@ -8,82 +8,12 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    private String separator;
 
-    public StringCalculator() {
-        separator = ",";
-    }
 
-    private void errorHandler(String expression) throws Exception {
-        int[] result = Stream.of(expression.split(separator))
+    public int operate(String expression) {
+        String[] expressionLines = expression.split("\n");
+        return Stream.of(expressionLines[0].split(","))
                 .mapToInt(Integer::parseInt)
-                .filter(n -> n < 0)
-                .toArray();
-        if (result.length > 0) {
-            throw new Exception("not allowed negatives: " + Arrays.toString(result));
-        }
-    }
-
-
-
-    public int operate(String expression) throws Exception {
-        if (expression.isEmpty()) {
-            return 0;
-        }
-        if (expression.contains("\n")) {
-            expression = determinateSeparator(expression);
-        }
-        String[] splitExpression = expression.split("\n");
-        return addition(splitExpression);
-    }
-
-    private int addition(String[] expressions) throws Exception {
-        int result = 0;
-        for (String expression : expressions) {
-            errorHandler(expression);
-            result += Stream.of(expression.split(separator))
-                    .mapToInt(Integer::parseInt)
-                    .filter(n -> n <= 1000)
-                    .sum();
-        }
-
-        return result;
-    }
-
-    private String determinateSeparator(String expression) {
-        String[] splitExpressionLines = expression.split("\n");
-        String singleSeparatorPattern = "(//)(\\[?[\\D]+]?)";
-        String multipleSeparatorPattern = "(\\[?[^/]([^\\[][*\\D]+[^]])]?)";
-        if (splitExpressionLines[0].matches(singleSeparatorPattern)) {
-            Pattern singleSeparator = Pattern.compile(singleSeparatorPattern);
-            Matcher singleSeparatorMatcher = singleSeparator.matcher(splitExpressionLines[0]);
-            Pattern multipleSeparator = Pattern.compile(multipleSeparatorPattern);
-            Matcher multipleSeparatorMatcher = multipleSeparator.matcher(splitExpressionLines[0]);
-            if (multipleSeparatorMatcher.find()) {
-                separator = multipleSeparatorMatcher.group();
-                separator = separator.substring(1, separator.length()-1);
-                String[] separators = separator.split("]\\[");
-                separator = "[";
-                for (String separator : separators) {
-                    this.separator += separator;
-                }
-                separator += "]+";
-                return expression.split("\n")[1];
-            }
-            if (singleSeparatorMatcher.find()) {
-                separator = singleSeparatorMatcher.group(2);
-                String escapableCharacter = "((\\[)?(\\*|\\+|\\.|\\^|\\{|\\||\\[|\\)|\\(\\?|])+(])?)+";
-                if (separator.matches(escapableCharacter)) {
-                    StringBuilder separatorBuilder = new StringBuilder();
-                    separator = separatorBuilder
-                            .append("\\")
-                            .append(separator.charAt(1))
-                            .append("+")
-                            .toString();
-                }
-            }
-            expression = splitExpressionLines[1];
-        }
-        return expression;
+                .sum();
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,4 +1,5 @@
 import com.leanmind.ciscoadiz.stringcalculator.StringCalculator;
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -6,45 +7,8 @@ import static org.junit.Assert.*;
 
 public class StringCalculatorShould {
     @Test
-    public void add_two_numbers_separated_by_commas() throws Exception {
+    public void operate_can_take_up_to_two_numbers_separated_by_commas() {
         int expected = 3;
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(expected, stringCalculator.operate("1,2"));
-    }
-    @Test
-    public void for_empty_string_should_not_add() throws Exception {
-        int expected = 0;
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(expected, stringCalculator.operate(""));
-    }
-    @Test
-    public void allow_to_handle_new_lines_between_numbers() throws Exception {
-        int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("1\n2,3"));
-    }
-    @Test
-    public void support_different_delimiters() throws Exception {
-        int expected = 3;
-        assertEquals(expected, new StringCalculator().operate("//;\n1;2"));
-    }
-    @Test
-    public void negative_numbers_not_allowed() {
-        Throwable exception = assertThrows(Exception.class, () -> new StringCalculator().operate("-1,5,-8"));
-        assertEquals("not allowed negatives: [-1, -8]", exception.getMessage());
-    }
-    @Test
-    public void numbers_bigger_than_1000_should_be_ignored() throws Exception {
-        int expected = 2;
-        assertEquals(expected, new StringCalculator().operate("1001,2"));
-    }
-    @Test
-    public void delimiters_can_be_of_any_length() throws Exception {
-        int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("//[***]\n1***2***3"));
-    }
-    @Test
-    public void allow_multiple_delimiters() throws Exception {
-        int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("//[*][$]\n1*2$3"));
+        assertEquals(expected, new StringCalculator().operate("1,2"));
     }
 }

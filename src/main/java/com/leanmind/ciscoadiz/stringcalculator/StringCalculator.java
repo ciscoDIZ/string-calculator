55c3aa7d90bfa605fb2379a7cfc9178fe39e7b98
ciscoadiz <ciscoadiz@gmail.com>
2022-04-19 19:31:26 +0100

    sum numbers in can take up to two numbers separated by commas

--- src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
+++ src/main/java/com/leanmind/ciscoadiz/stringcalculator/StringCalculator.java
@@ -9,64 +9,12 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    private String separator;
 
-    public StringCalculator() {
-        separator = ",";
+    public int sumNumbersIn(String expression) {
+        Integer[] numbers = Stream.of(expression.split(","))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
+        return numbers[0] + numbers[1];
     }
 
-    private int addition(String[] expressions, int i) throws NegativesNotAllowed {
-        if (expressions.length < 1) {
-            return 0;
-        }
-        String[] next = Arrays.copyOfRange(expressions, 0, expressions.length - 1);
-        String exceptionMessage = "you can't add up negative numbers. affected numbers:";
-        errorHandler(exceptionMessage, expressions[i]);
-        int currentValue = Stream.of(expressions[i].split(separator))
-                .mapToInt(Integer::parseInt)
-                .filter(n -> !(n > 1000))
-                .sum();
-        return addition(next, i-1) + currentValue;
-    }
-
-    private void errorHandler(String exceptionMessage, String expressions) throws NegativesNotAllowed {
-        int[] negatives = Stream.of(expressions.split(separator))
-                .mapToInt(Integer::parseInt)
-                .filter(n-> n < 0)
-                .toArray();
-        if (negatives.length > 0) {
-            throw new NegativesNotAllowed(exceptionMessage, negatives);
-        }
-    }
-
-    public int operate(String expression) throws NegativesNotAllowed {
-        String[] expressionLines = expression.split("\n");
-        expressionLines = determinateSeparator(expressionLines);
-        return addition(expressionLines, expressionLines.length-1);
-    }
-
-    private String[] determinateSeparator(String[] expressionLines) {
-        Pattern separatorPatter = Pattern.compile("(//)(\\[?[^A-z0-9]+]?)+");
-        Matcher separatorMatcher = separatorPatter.matcher(expressionLines[0]);
-
-        if (separatorMatcher.find()) {
-            this.separator = separatorMatcher.group(0);
-            this.separator = this.separator.substring(2);
-            if (this.separator.matches("[\\[][\\D]+[]]")) {
-                this.separator = this.separator.substring(1, this.separator.length()-1);
-            }
-            String[] separators = separator.split("]\\[");
-            if (separators.length > 1) {
-                separator = Stream.of(separators).reduce("", String::concat);
-                separator = "["+separator+"]";
-                expressionLines = Arrays.copyOfRange(expressionLines, 1, expressionLines.length);
-                return expressionLines;
-            }
-            if (this.separator.length() > 1) {
-                this.separator = "["+this.separator.charAt(0)+"]{"+this.separator.length()+"}";
-            }
-            expressionLines = Arrays.copyOfRange(expressionLines, 1, expressionLines.length);
-        }
-        return expressionLines;
-    }
 }
--- src/test/java/StringCalculatorShouldTest.java
+++ src/test/java/StringCalculatorShouldTest.java
@@ -10,24 +10,29 @@ public class StringCalculatorShouldTest {
     @Test
     public void operate_can_take_up_to_two_numbers_separated_by_commas() throws NegativesNotAllowed {
         int expected = 3;
-        assertEquals(expected, new StringCalculator().operate("1,2"));
+        assertEquals(expected, new StringCalculator().sumNumbersIn("1,2"));
+    }
+    /*@Test
+    public void sum_numbers_in_allow_sum_indeterminate_numbers_separated_by_commas() {
+        int expected = 6;
+        assertEquals(expected, new StringCalculator().sumNumbersIn("1,2,3"));
     }
     @Test
     public void allow_the_operate_method_to_handle_new_lines_between_numbers() throws NegativesNotAllowed {
         int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("1\n2,3"));
+        assertEquals(expected, new StringCalculator().sumNumbersIn("1\n2,3"));
     }
     @Test
     public void allow_different_separator() throws NegativesNotAllowed {
         int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("//;\n1;2;3"));
+        assertEquals(expected, new StringCalculator().sumNumbersIn("//;\n1;2;3"));
 
     }
     @Test
     public void negative_number_will_throw_an_exception_negatives_not_allowed() {
         Throwable negativesNotAllowed = assertThrows(
                 NegativesNotAllowed.class,
-                () -> new StringCalculator().operate("2,-8,1,-5")
+                () -> new StringCalculator().sumNumbersIn("2,-8,1,-5")
         );
         assertEquals(
                 "you can't add up negative numbers. affected numbers: [-8, -5]",
@@ -37,16 +42,16 @@ public class StringCalculatorShouldTest {
     @Test
     public void numbers_bigger_than_1000_should_be_ignored() throws NegativesNotAllowed {
         int expected = 2;
-        assertEquals(expected, new StringCalculator().operate("1001,2"));
+        assertEquals(expected, new StringCalculator().sumNumbersIn("1001,2"));
     }
     @Test
     public void delimiters_can_be_of_any_length() throws NegativesNotAllowed {
         int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("//[***]\n1***2***3"));
+        assertEquals(expected, new StringCalculator().sumNumbersIn("//[***]\n1***2***3"));
     }
     @Test
     public void allow_multiple_delimiters() throws NegativesNotAllowed {
         int expected = 6;
-        assertEquals(expected, new StringCalculator().operate("//[;][*]\n1;2*3"));
-    }
+        assertEquals(expected, new StringCalculator().sumNumbersIn("//[;][*]\n1;2*3"));
+    }*/
 }
